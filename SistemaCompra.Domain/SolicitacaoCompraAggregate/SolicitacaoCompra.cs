e62da7a8431f37f96c16fba7e0b669b68223f87a
Milton Lima <milton.mlima@gmail.com>
2026-03-19 15:58:01 -0300

    A entidade SolicitacaoCompra no domínio, já está implementada, adicione método necessário de forma a garantir que as seguintes regras de negócio sejam validadas:    a. Se o Total Geral for maior que 50000 a condição de pagamento deve ser 30 dias.    b. O total de itens de compra deve ser maior que 0.

--- SistemaCompra.Domain/SolicitacaoCompraAggregate/SolicitacaoCompra.cs
+++ SistemaCompra.Domain/SolicitacaoCompraAggregate/SolicitacaoCompra.cs
@@ -1,4 +1,5 @@
-﻿using SistemaCompra.Domain.Core.Model;
+﻿using SistemaCompra.Domain.Core;
+using SistemaCompra.Domain.Core.Model;
 using SistemaCompra.Domain.ProdutoAggregate;
 using System;
 using System.Collections.Generic;
@@ -37,8 +38,26 @@ namespace SistemaCompra.Domain.SolicitacaoCompraAggregate
         public void RegistrarCompra(IEnumerable<Item> itens)
         {
             var itensRegistrados = itens?.ToList() ?? new List<Item>();
+            var totalItens = itensRegistrados.Sum(i => i.Qtde);
+            var totalGeral = itensRegistrados.Sum(i => i.Subtotal.Value);
+
+            ValidarRegrasCompra(totalItens, totalGeral);
+
             Itens = itensRegistrados;
-            TotalGeral = new Money(itensRegistrados.Sum(i => i.Subtotal.Value));
+            TotalGeral = new Money(totalGeral);
+        }
+
+        private void ValidarRegrasCompra(int totalItens, decimal totalGeral)
+        {
+            if (totalItens <= 0)
+            {
+                throw new BusinessRuleException("O total de itens de compra deve ser maior que 0.");
+            }
+
+            if (totalGeral > 50000 && CondicaoPagamento.Valor != 30)
+            {
+                throw new BusinessRuleException("Para compras acima de 50000, a condição de pagamento deve ser 30 dias.");
+            }
         }
     }
 }
